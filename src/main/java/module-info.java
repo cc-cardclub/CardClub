883887716f28c9a0e517cf37d35060e4cc960b50
Reither Bernd <bernd.reither@htl.rennweg.at>
2022-02-15 09:32:39 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module at.rennweg.htl.cardclubclient {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens at.rennweg.htl.cardclubclient to javafx.fxml;
+    exports at.rennweg.htl.cardclubclient;
+}
