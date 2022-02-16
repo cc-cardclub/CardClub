0d454b17fc8a13c531bdaa4c4c7edf3379ccd5b9
Hoermann Lisa-Marie <0198@htl.rennweg.at>
2022-02-16 16:41:50 +0100

    prototype gameboard

--- src/main/java/at/rennweg/htl/cardclubclient/HelloApplication.java
+++ src/main/java/at/rennweg/htl/cardclubclient/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("spielbrett_v1.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 720, 540);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
