32512ab97f320f5cea158b47f0da04364c02de8c
Reither Bernd <bernd.reither@htl.rennweg.at>
2022-02-18 11:34:41 +0100

    added Card prototype

--- src/main/java/at/rennweg/htl/cardclubclient/GameBoardController.java
+++ src/main/java/at/rennweg/htl/cardclubclient/GameBoardController.java
@@ -1,16 +1,28 @@
 package at.rennweg.htl.cardclubclient;
 
+import at.rennweg.htl.cardclubclient.cards.Card;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 public class GameBoardController {
     @FXML
     private Button UNOButton;
+    @FXML
+    private HBox handCards;
 
     @FXML
     protected void onUNOButtonClick() {
         UNOButton.setText("Clicked");
+
+        Card newCard = new Card(5, "green");
+        ImageView cardImg = new ImageView(String.valueOf(GameBoard.class.getResource(newCard.getTexture())));
+        cardImg.setFitHeight(100D);
+        cardImg.setFitWidth(70D);
+
+        handCards.getChildren().add(cardImg);
     }
 
     @FXML
