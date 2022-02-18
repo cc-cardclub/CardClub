6691fb67d38f36930fa8ea2d2ab93c353c147ce6
Hoermann Lisa-Marie <0198@htl.rennweg.at>
2022-02-18 09:59:19 +0100

    Corrected GameBoard classes

--- src/main/java/at/rennweg/htl/cardclubclient/GameBoard.java
+++ src/main/java/at/rennweg/htl/cardclubclient/GameBoard.java
@@ -1,8 +1,6 @@
 package at.rennweg.htl.cardclubclient;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -12,7 +10,7 @@ import java.io.IOException;
 public class GameBoard extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("spielbrett_v1.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GameBoard.class.getResource("spielbrett_v1.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Play UNO");
         stage.setScene(scene);
