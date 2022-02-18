5c4b9d495d8c36d24da4bee21d00b42be13bbc1e
Mattias Burkard <mattias.burkard@htl.rennweg.at>
2022-02-18 16:55:13 +0100

    Basic card deck implementation

---
+++ src/main/java/at/rennweg/htl/cardclubclient/cards/Deck.java
@@ -0,0 +1,45 @@
+package at.rennweg.htl.cardclubclient.cards;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Deck {
+    private static List<Card> remainingCards = new ArrayList<>();
+    private static List<Card> playedCards = new ArrayList<>();
+
+    public static void playCard(Card card) {
+        // TODO check validity
+        playedCards.add(card);
+    }
+
+    public static Card getLastCard() {
+        if (playedCards.size() == 0) {
+            return null;
+        }
+        return playedCards.get(playedCards.size() - 1);
+    }
+
+    public static Card drawCard() {
+        if (remainingCards.size() < 1) {
+            // TODO leave last played card
+            shuffleCards(playedCards);
+        }
+
+        Card drawnCard = remainingCards.get(0);
+        remainingCards.remove(drawnCard);
+
+        return drawnCard;
+    }
+
+    private static List<Card> shuffleCards(List<Card> cards) {
+        List<Card> shuffledCards = new ArrayList<>();
+        while (cards.size() > 0) {
+            // TODO actually shuffle
+            Random random = new Random();
+            random.nextInt();
+        }
+
+        return shuffledCards;
+    }
+}
