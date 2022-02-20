1f8b08feb049afc0b091887708d3adea1726aae0
Reither Bernd <bernd.reither@htl.rennweg.at>
2022-02-20 21:30:04 +0100

    display the first card of the deck on the gameboard, preparations for PlayerCards class

--- src/main/java/at/rennweg/htl/cardclubclient/GameBoardController.java
+++ src/main/java/at/rennweg/htl/cardclubclient/GameBoardController.java
@@ -3,17 +3,33 @@ package at.rennweg.htl.cardclubclient;
 import at.rennweg.htl.cardclubclient.cards.Card;
 import at.rennweg.htl.cardclubclient.cards.Deck;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
+import java.net.URL;
 import java.util.Random;
+import java.util.ResourceBundle;
 
-public class GameBoardController {
+public class GameBoardController implements Initializable {
+    @FXML
+    private ImageView discardPileImg;
     @FXML
     private Button UNOButton;
     @FXML
     private HBox handCards;
+
+    private Card selectedCard;
+
+    @FXML
+    public void initialize(URL location, ResourceBundle resources) {
+        Image img = new Image(String.valueOf(
+                        GameBoardController.class.getResource(Deck.getLastCard().getTexture())
+        ));
+        discardPileImg.setImage(img);
+    }
 
     @FXML
     protected void onDrawPile() {
@@ -27,6 +43,11 @@ public class GameBoardController {
 
     @FXML
     protected void onDiscardPile() {
+
+    }
+
+    @FXML
+    protected void onCardSelect() {
 
     }
 
