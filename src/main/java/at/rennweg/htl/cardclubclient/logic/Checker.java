0076009aa8d75b1dee45c2cbf8e3063bdbbe9d11
Mattias Burkard <mattias.burkard@htl.rennweg.at>
2022-02-21 08:00:49 +0100

    Improved turn validity checks

--- src/main/java/at/rennweg/htl/cardclubclient/logic/Checker.java
+++ src/main/java/at/rennweg/htl/cardclubclient/logic/Checker.java
@@ -4,8 +4,10 @@ import at.rennweg.htl.cardclubclient.cards.Card;
 
 public class Checker {
     public static boolean checkTurnValidity(Card currentCard, Card previousCard) {
-        // TODO check if special
-        if (currentCard.getNumber() == previousCard.getNumber()) {
+        if (currentCard.getColor().equals("black")) {
+            return true;
+        }
+        if (currentCard.getNumber().equals(previousCard.getNumber())) {
             return true;
         }
         if (currentCard.getColor().equals(previousCard.getColor())) {
