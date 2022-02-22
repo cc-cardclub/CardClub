2ac5fb31688c9ed2b8f92c5f459a664a3ca7fcdb
Hoermann Lisa-Marie <0198@htl.rennweg.at>
2022-02-22 08:33:16 +0100

    Color-Selector closes automatically

--- src/main/java/at/rennweg/htl/cardclubclient/ColorPopUpController.java
+++ src/main/java/at/rennweg/htl/cardclubclient/ColorPopUpController.java
@@ -2,22 +2,36 @@ package at.rennweg.htl.cardclubclient;
 
 import at.rennweg.htl.cardclubclient.cards.Deck;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class ColorPopUpController {
+    @FXML
+    private AnchorPane ColorPopUp;
 
     public void onBlueColorSelected(ActionEvent actionEvent) {
         Deck.getLastCard().setColor("blue");
+        closeWindow();
     }
 
     public void onRedColorSelected(ActionEvent actionEvent) {
         Deck.getLastCard().setColor("red");
+        closeWindow();
     }
 
     public void onYellowColorSelected(ActionEvent actionEvent) {
         Deck.getLastCard().setColor("yellow");
+        closeWindow();
     }
 
     public void onGreenColorSelected(ActionEvent actionEvent) {
         Deck.getLastCard().setColor("green");
+        closeWindow();
+    }
+
+    private void closeWindow() {
+        Stage stage = (Stage) ColorPopUp.getScene().getWindow();
+        stage.close();
     }
 }
