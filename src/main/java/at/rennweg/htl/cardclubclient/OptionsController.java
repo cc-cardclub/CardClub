f61cc8e6b77161313e61e2fff1d93f26d407f97d
Hoermann Lisa-Marie <0198@htl.rennweg.at>
2022-03-11 20:49:15 +0100

    Checkstyle in OptionsController

--- src/main/java/at/rennweg/htl/cardclubclient/OptionsController.java
+++ src/main/java/at/rennweg/htl/cardclubclient/OptionsController.java
@@ -5,12 +5,25 @@ import javafx.fxml.FXML;
 
 import java.io.IOException;
 
+/**
+ * class to close the OptionsMenu
+ *
+ * @author Bernd Reither
+ */
 public class OptionsController {
+    /**
+     * return to StartMenu
+     *
+     * @throws IOException
+     */
     @FXML
     protected void onReturnToStartmenuButton() throws IOException {
         Startmenu.start();
     }
 
+    /**
+     * reset the game
+     */
     @FXML
     protected void onResetGameCore() {
         GameCore.reset();
