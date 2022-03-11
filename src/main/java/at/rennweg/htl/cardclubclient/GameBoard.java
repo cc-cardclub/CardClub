df99bda0f174f47f63ac8f3b8ede3196f3a0e744
Hoermann Lisa-Marie <0198@htl.rennweg.at>
2022-03-11 10:37:23 +0100

    checkstyle in GameBoard Class

--- src/main/java/at/rennweg/htl/cardclubclient/GameBoard.java
+++ src/main/java/at/rennweg/htl/cardclubclient/GameBoard.java
@@ -6,13 +6,29 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * set the stage of our GameBoard to play UNO
+ *
+ * @author Lisa-Marie Hörmann, Bernd Reither
+ */
 public class GameBoard {
+    /**
+     * set the controller of the GameBoard
+     */
     private static GameBoardController controller;
 
+    /**
+     * set the controller of the GameBoard
+     * @param controller
+     */
     public void setController(GameBoardController controller) {
-        GameBoard.controller = controller ;
+        GameBoard.controller = controller;
     }
 
+    /**
+     * for Main to launch the new stage after Singleplayer-Menu
+     * @throws IOException
+     */
     public static void start() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GameBoard.class.getResource("spielbrett_v1.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
@@ -22,11 +38,16 @@ public class GameBoard {
         stage.show();
     }
 
-
+    /**
+     * for controller to end a Bot turn
+     */
     public static void endBotTurn() {
         controller.endBotTurn();
     }
 
+    /**
+     * for controller to refresh the board after game finished
+     */
     public static void refresh() {
         controller.refresh();
     }
