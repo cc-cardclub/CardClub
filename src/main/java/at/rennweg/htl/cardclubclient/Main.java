8673832559f3fc6dbe04a56fd25188734a059b7d
Hoermann Lisa-Marie <0198@htl.rennweg.at>
2022-03-11 20:54:33 +0100

    Checkstyle in Main

--- src/main/java/at/rennweg/htl/cardclubclient/Main.java
+++ src/main/java/at/rennweg/htl/cardclubclient/Main.java
@@ -3,19 +3,43 @@ package at.rennweg.htl.cardclubclient;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * change between stages
+ *
+ * @author Bernd Reither, Mattias Burkard
+ */
 public class Main extends Application {
+    /**
+     * store the current stage
+     */
     private static Stage primaryStage;
 
+    /**
+     * get the current stage
+     *
+     * @return current stage
+     */
     public static Stage getStage() {
         return primaryStage;
     }
 
+    /**
+     * set the stage
+     *
+     * @param primaryStage set stage (GameBoard, Menu)
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         Main.primaryStage = primaryStage;
         Startmenu.start();
     }
 
+    /**
+     * launch the current stage
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         launch(args);
     }
