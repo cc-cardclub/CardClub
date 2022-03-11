d9b95664ab6ba403a689e5a2868ab65e9c05740a
Hoermann Lisa-Marie <0198@htl.rennweg.at>
2022-03-11 20:42:49 +0100

    Checkstyle in colorPopUpController

--- src/main/java/at/rennweg/htl/cardclubclient/ColorPopUpController.java
+++ src/main/java/at/rennweg/htl/cardclubclient/ColorPopUpController.java
@@ -6,32 +6,63 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+/**
+ * choose the color when you play a wild-card
+ *
+ * @author Lisa-Marie
+ */
 public class ColorPopUpController {
+    /**
+     * stage with the colour buttons
+     */
     @FXML
-    private AnchorPane ColorPopUp;
+    private AnchorPane colorPopUp;
 
+    /**
+     * choose the color blue and set it as the next card on the pile
+     *
+     * @param actionEvent
+     */
     public void onBlueColorSelected(ActionEvent actionEvent) {
         Deck.getLastCard().setColor("blue");
         closeWindow();
     }
 
+    /**
+     * choose the color red and set it as the next card on the pile
+     *
+     * @param actionEvent
+     */
     public void onRedColorSelected(ActionEvent actionEvent) {
         Deck.getLastCard().setColor("red");
         closeWindow();
     }
 
+    /**
+     * choose the color yellow and set it as the next card on the pile
+     *
+     * @param actionEvent
+     */
     public void onYellowColorSelected(ActionEvent actionEvent) {
         Deck.getLastCard().setColor("yellow");
         closeWindow();
     }
 
+    /**
+     * choose the color green and set it as the next card on the pile
+     *
+     * @param actionEvent
+     */
     public void onGreenColorSelected(ActionEvent actionEvent) {
         Deck.getLastCard().setColor("green");
         closeWindow();
     }
 
+    /**
+     * close the color-choose-window
+     */
     private void closeWindow() {
-        Stage stage = (Stage) ColorPopUp.getScene().getWindow();
+        Stage stage = (Stage) colorPopUp.getScene().getWindow();
         stage.close();
     }
 }
