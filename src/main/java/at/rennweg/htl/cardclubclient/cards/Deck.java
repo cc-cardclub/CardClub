6586f397fc0e2417491956a910ea0e268e0d20ca
Mattias Burkard <mattias.burkard@htl.rennweg.at>
2022-03-21 08:27:49 +0100

    Deck JavaDoc and code cleanup

--- src/main/java/at/rennweg/htl/cardclubclient/cards/Deck.java
+++ src/main/java/at/rennweg/htl/cardclubclient/cards/Deck.java
@@ -6,13 +6,36 @@ import at.rennweg.htl.cardclubclient.logic.GameCore;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
+/**
+ * Class containing the card deck for the game
+ *
+ * @author Mattias Burkard
+ */
 public class Deck {
+    /**
+     * Remaining cards in the deck
+     */
     private static List<Card> remainingCards = new ArrayList<>();
-    private static List<Card> playedCards = new ArrayList<>();
+
+    /**
+     * Already played cards
+     */
+    private static final List<Card> playedCards = new ArrayList<>();
+
+    /**
+     * Amount of cards that the next player needs to draw
+     */
     private static int drawAmount = 0;
 
+    /**
+     * Method for playing a card
+     *
+     * @param playerCards all cards which the player has
+     * @param card card that the player wants to play
+     */
     public static void playCard(Player playerCards, Card card) {
         if (Checker.checkTurnValidity(card, getLastCard())) {
             doTurn(playerCards, card);
@@ -22,20 +45,34 @@ public class Deck {
         }
     }
 
+    /**
+     * Method for playing a card and selecting a color
+     * @param playerCards all cards which the player has
+     * @param card card that the player wants to play
+     * @param color color which should be chosen if the card was a wild card
+     */
     public static void playCard(Player playerCards, Card card, String color) {
         if (Checker.checkTurnValidity(card, getLastCard())) {
             doTurn(playerCards, card);
             if (card.getColor().equals("black")) {
                 if (color.equals("black")) {
+                    final int colorAmount = 4;
+                    final int firstCase = 0;
+                    final int secondCase = 1;
+                    final int thirdCase = 2;
+                    final int fourthCase = 3;
+
                     Random random = new Random();
-                    switch (random.nextInt(4)) {
-                        case 0 -> getLastCard().setColor("yellow");
-                        case 1 -> getLastCard().setColor("blue");
-                        case 2 -> getLastCard().setColor("green");
-                        case 3 -> getLastCard().setColor("red");
+
+                    switch (random.nextInt(colorAmount)) {
+                        case firstCase -> Objects.requireNonNull(getLastCard()).setColor("yellow");
+                        case secondCase -> Objects.requireNonNull(getLastCard()).setColor("blue");
+                        case thirdCase -> Objects.requireNonNull(getLastCard()).setColor("green");
+                        case fourthCase -> Objects.requireNonNull(getLastCard()).setColor("red");
+                        default -> Objects.requireNonNull(getLastCard()).setColor("black");
                     }
                 } else {
-                    getLastCard().setColor(color);
+                    Objects.requireNonNull(getLastCard()).setColor(color);
                 }
             }
         } else {
@@ -45,7 +82,7 @@ public class Deck {
     }
 
     private static void doTurn(Player playerCards, Card card) {
-        if (getLastCard().getNumber().equals("draw2")) {
+        if (Objects.requireNonNull(getLastCard()).getNumber().equals("draw2")) {
             if (GameCore.plus2and4CardsSelected) {
                 if (!card.getNumber().equals("draw2") && !card.getNumber().equals("wildDraw4")) {
                     playerCards.addCard(getCards(drawAmount));
@@ -78,22 +115,32 @@ public class Deck {
         playedCards.add(card);
         playerCards.removeCard(card);
         if (card.isSpecial()) {
-            if (card.getNumber().equals("skip")) {
-                GameCore.switchToNextPlayer();
-            } else if (card.getNumber().equals("draw2")) {
-                drawAmount += 2;
-            } else if (card.getNumber().equals("wildDraw4")) {
-                drawAmount += 4;
-            } else if (card.getNumber().equals("reverse")) {
-                if (GameCore.getPlayers().size() == 2) {
+            switch (card.getNumber()) {
+                case "skip":
                     GameCore.switchToNextPlayer();
-                } else {
-                    GameCore.reverseDirection();
-                }
+                    break;
+                case "draw2":
+                    drawAmount += 2;
+                    break;
+                case "wildDraw4":
+                    final int draw4Amount = 4;
+                    drawAmount += draw4Amount;
+                    break;
+                case "reverse":
+                    if (GameCore.getPlayers().size() == 2) {
+                        GameCore.switchToNextPlayer();
+                    } else {
+                        GameCore.reverseDirection();
+                    }
+                    break;
             }
         }
     }
 
+    /**
+     * Method for getting the last played card
+     * @return the last played card
+     */
     public static Card getLastCard() {
         if (playedCards.size() == 0) {
             return null;
@@ -101,6 +148,10 @@ public class Deck {
         return playedCards.get(playedCards.size() - 1);
     }
 
+    /**
+     * Method for drawing a card
+     * @return the next available card
+     */
     public static Card drawCard() {
         if (remainingCards.size() < 1) {
             List<Card> shuffleThis = new ArrayList<>();
@@ -138,6 +189,9 @@ public class Deck {
         return shuffledCards;
     }
 
+    /**
+     * Method for preparing a new deck and adding all cards to it
+     */
     public static void prepareDeck() {
         List<Card> deck = new ArrayList<>();
         String[] colors = {"yellow", "red", "green", "blue", "black"};
@@ -152,17 +206,19 @@ public class Deck {
 
     private static List<Card> getAllCardsWithColor(String color) {
         List<Card> cards = new ArrayList<>();
+        final int wildCardAmount = 4;
+        final int numberCards = 10;
 
         if (color.equals("black")) {
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < wildCardAmount; i++) {
                 cards.add(new Card("wild", color, true));
                 cards.add(new Card("wildDraw4", color, true));
             }
         } else {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < numberCards; i++) {
                 cards.add(new Card(i + "", color, false));
             }
-            for (int i = 1; i < 10; i++) {
+            for (int i = 1; i < numberCards; i++) {
                 cards.add(new Card(i + "", color, false));
             }
 
@@ -176,10 +232,19 @@ public class Deck {
         return cards;
     }
 
+    /**
+     * Method for getting the starting cards for a player
+     * @return an array of cards for the player
+     */
     public static Card[] getPlayerStartCards() {
         return getCards(GameCore.getStartingCards());
     }
 
+    /**
+     * Method for getting a specific amount of cards
+     * @param amount amount of cards to get
+     * @return an array of cards for the player
+     */
     public static Card[] getCards(int amount) {
         Card[] cards = new Card[amount];
 
