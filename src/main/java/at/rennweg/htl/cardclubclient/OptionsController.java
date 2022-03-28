9ba0325a32034ede8ef286718ae70a5b07094fa4
Reither Bernd <bernd.reither@htl.rennweg.at>
2022-03-28 09:16:30 +0200

    added properties in OptionsController

--- src/main/java/at/rennweg/htl/cardclubclient/OptionsController.java
+++ src/main/java/at/rennweg/htl/cardclubclient/OptionsController.java
@@ -2,15 +2,40 @@ package at.rennweg.htl.cardclubclient;
 
 import at.rennweg.htl.cardclubclient.logic.GameCore;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Slider;
+import javafx.scene.input.TouchEvent;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 /**
  * class to close the OptionsMenu
  *
  * @author Bernd Reither
  */
-public class OptionsController {
+public class OptionsController implements Initializable {
+    @FXML
+    private Slider volumeSlider;
+
+    private final Properties props = getProps();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        volumeSlider.adjustValue(Double.parseDouble(props.getProperty("volume")));
+
+        // onVolumeSlider
+        volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            props.setProperty("volume", String.valueOf(volumeSlider.getValue()));
+        });
+    }
+
     /**
      * return to StartMenu
      *
@@ -27,5 +52,17 @@ public class OptionsController {
     @FXML
     protected void onResetGameCore() {
         GameCore.reset();
+    }
+
+    private Properties getProps() {
+        Properties props = new Properties();
+
+        try {
+            props.load(AboutController.class.getResourceAsStream("data/settings.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return props;
     }
 }
