b74cd063c288fc0d0c2c81247f08336912658dec
Reither Bernd <bernd.reither@htl.rennweg.at>
2022-04-05 09:39:08 +0200

    insert data into table

--- src/main/java/at/rennweg/htl/cardclubclient/LeaderboardController.java
+++ src/main/java/at/rennweg/htl/cardclubclient/LeaderboardController.java
@@ -1,8 +1,41 @@
 package at.rennweg.htl.cardclubclient;
 
-import java.io.IOException;
+import at.rennweg.htl.cardclubclient.logic.ServerConnection;
+import at.rennweg.htl.cardclubserver.User;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class LeaderboardController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class LeaderboardController implements Initializable {
+    @FXML
+    private TableView<User> leaderboard;
+    @FXML
+    private TableColumn<User, String> playerColumn;
+    @FXML
+    private TableColumn<User, String> winsColumn;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ServerConnection.leaderboardWin();
+        System.out.println(ServerConnection.getLeaderboard());
+
+        List<User> leaderboardList = ServerConnection.getLeaderboard();
+
+        playerColumn.setCellValueFactory((data) -> new SimpleStringProperty(data.getValue().getName()));
+        winsColumn.setCellValueFactory((data) -> new SimpleStringProperty(String.valueOf(data.getValue().getWins())));
+
+        leaderboard.getItems().addAll(leaderboardList);
+    }
+
     /**
      * Event for button to return to the start menu
      *
